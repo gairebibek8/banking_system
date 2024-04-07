174ebf9a2d17647bc3c9ae2ef40b140850a91d59
Bibek Gaire <gairebibek8@gmail.com>
2024-04-07 11:37:32 -0500

    Updated signup page and config fiel to check if database connection

---
+++ src/config.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const connect = mongoose.connect("mongodb://localhost:27017/login");
+
+// check databse connection
+connect.then(() => {
+    console.log("Database connected successfully");
+})
+.catch(() => {
+    console.log("Database could not be connected");
+});
+
+// Create Schema
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// collection part
+const collection = new mongoose.model("users", LoginSchema);
+
+module.exports = collection;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require("path");
 const bcrypt = require("bcrypt");
-
+const collection = require("./config");
 
 const app = express();
 
+// convert data into json format
+app.use(express.json());
+
+
+app.use(express.urlencoded({ extended: false }));
 
 // set EJS as view engine
 app.set('view engine', 'ejs');
@@ -23,6 +28,20 @@ app.get("/signup", (req, res) => {
 });
 
 
+// Register User
+app.post("/signup", async (req, res) => {
+
+    const data = {
+        name: req.body.username,
+        password: req.body.password
+    }
+
+    const userdata = await collection.insertOne(data);
+    console.log(userdata);
+});
+
+
+// define port connection for user
 const port = 5000;
 app.listen(port, () => {
     console.log(`Server running on Port: ${port}`);
