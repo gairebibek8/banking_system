ac5931f88600bcfb0d6598059908471633051164
Bibek Gaire <gairebibek8@gmail.com>
2024-04-07 13:49:10 -0500

    Login and sign up page works successfully

--- src/index.js
+++ src/index.js
@@ -36,8 +36,45 @@ app.post("/signup", async (req, res) => {
         password: req.body.password
     }
 
-    const userdata = await collection.insertOne(data);
+    // Check if the username already exists in the database
+    const existingUser = await collection.findOne({ name: data.name });
+
+    if (existingUser) {
+        res.send('User already exists. Please choose a different username.');
+    } else {
+        // Hash the password using bcrypt
+        const saltRounds = 10; // Number of salt rounds for bcrypt
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
+        data.password = hashedPassword; // Replace the original password with the hashed one
+
+    const userdata = await collection.insertMany(data);
     console.log(userdata);
+
+    }
+});
+
+
+
+// Login user 
+app.post("/login", async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.username });
+        if (!check) {
+            res.send("User name cannot found")
+        }
+        // Compare the hashed password from the database with the plaintext password
+        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+        if (!isPasswordMatch) {
+            res.send("wrong Password");
+        }
+        else {
+            res.render("home");
+        }
+    }
+    catch {
+        res.send("wrong Details");
+    }
 });
 
 
